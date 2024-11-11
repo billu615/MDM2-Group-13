9990497b067bf70694f1a050093ec4f96973d80f
hehe1207 <168436705+hehe1207@users.noreply.github.com>
2024-11-11 15:17:39 +0000

    Update Fourier.py

--- Fourier/Fourier.py
+++ Fourier/Fourier.py
@@ -21,22 +21,22 @@ filtered_shifted = f_transform_shifted * mask
 filtered_image = np.fft.ifft2(np.fft.ifftshift(filtered_shifted))
 filtered_image = np.abs(filtered_image)
 
-plt.figure(figsize=(18, 6))
+plt.figure()
 
 plt.subplot(1, 3, 1)
-plt.title("Original Image")
+plt.title("Original Image", fontsize = 12)
 plt.imshow(image, cmap='gray')
 plt.axis('off')
 
 
 plt.subplot(1, 3, 2)
-plt.title("Magnitude Spectrum")
+plt.title("Magnitude Spectrum", fontsize = 12)
 plt.imshow(magnitude_spectrum, cmap='gray')
 plt.axis('off')
 
 
 plt.subplot(1, 3, 3)
-plt.title("Filtered Image (Low-Pass)")
+plt.title("Filtered Image (Low-Pass)", fontsize = 12)
 plt.imshow(filtered_image, cmap='gray')
 plt.axis('off')
 
